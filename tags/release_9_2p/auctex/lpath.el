57e914caf1f9028e3f6995413d769db445ce91c1
(no author) <(no author)@b9a4a032-1d71-47fc-9e6e-57b8675ed99c>
1994-12-02 09:06:53 +0000

    This commit was manufactured by cvs2svn to create tag 'release_9_2p'.
    git-svn-id: file:///home/cassou/tmp/auctex-repo/auctex.svn@426 b9a4a032-1d71-47fc-9e6e-57b8675ed99c

---
+++ tags/release_9_2p/auctex/lpath.el
@@ -0,0 +1,7 @@
+;;; This file is only used for installing AUC TeX.
+;;; It is not a part of AUC TeX itself.
+
+;; Make sure we get the right files.
+(setq load-path (cons "." load-path)
+      TeX-lisp-directory "<none>")
+
