cdf591c0c0c9ac719f4ab38b8ba08bc6da0f62f2
(no author) <(no author)@b9a4a032-1d71-47fc-9e6e-57b8675ed99c>
2002-04-17 01:48:38 +0000

    This commit was manufactured by cvs2svn to create tag 'rel-0-7-1'.
    git-svn-id: file:///home/cassou/tmp/auctex-repo/auctex.svn@1812 b9a4a032-1d71-47fc-9e6e-57b8675ed99c

---
+++ tags/rel-0-7-1/auctex/preview/autogen.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+autoconf
+grep -q '^MY_LANG=' configure || {
+  echo "Trying to deal with outdated autoconf"
+# Two fixes: Save LANG and LC_CTYPE before configure munges it
+# Account for difference between AC_INIT in 2.13 and 2.52
+  ed -s configure <<\EOF
+/^[^#]/i
+MY_LANG=$LANG
+MY_LC_CTYPE=$LC_CTYPE
+.
+,s/ac_unique_file=preview-latex/ac_unique_file=preview-latex.el/
+w
+q
+EOF
+}
+cd doc
+makeinfo -D rawfile --no-headers --no-validate readme.texi >../README
+makeinfo -D rawfile --no-headers --no-validate install.texi >../INSTALL
+makeinfo -D rawfile --no-headers --no-validate problems.texi >../PROBLEMS
+makeinfo -D rawfile --no-headers --no-validate todo.texi >../TODO
+cd ..
